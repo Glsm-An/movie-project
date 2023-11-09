65920e9be006053de7bd29980d2584159a273dc2
An <1635853884@qq.com>
2023-11-09 19:29:33 +0800

    完成项目环境搭建如axios,vantUI库等

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import vantConfig from './untils/vant-config'
+
 Vue.config.productionTip = false
+
+Vue.use(vantConfig)
 
 new Vue({
   router,
---
+++ src/untils/vant-config.js
@@ -0,0 +1,7 @@
+import { } from 'vant';
+
+export default {
+    install(Vue) {
+
+    }
+}
